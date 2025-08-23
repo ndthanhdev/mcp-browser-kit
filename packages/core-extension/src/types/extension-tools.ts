5012e9893a45f4ca5ae6063adba01b9ed6e670ae
ndthanhdev <ndthanhdev@outlook.com>
2025-08-23 19:18:55 +0700

    feat: add Selection type and extend ExtensionTools interface with getSelection method

--- packages/core-extension/src/types/extension-tools.ts
+++ packages/core-extension/src/types/extension-tools.ts
@@ -1,5 +1,6 @@
 import type { ExtensionContext } from "./extension-context";
 import type { Screenshot } from "./screenshot";
+import type { Selection } from "./selection";
 
 export interface ExtensionTools {
 	getExtensionContext(): Promise<ExtensionContext>;
@@ -10,45 +11,46 @@ export interface ExtensionTools {
 	// 	windowId: string,
 	// 	tabId: string
 	// ): Promise<ElementRecord[]>;
+	getSelection(windowId: string, tabId: string): Promise<Selection>;
 	clickOnViewableElement(
 		windowId: string,
 		tabId: string,
 		x: number,
-		y: number
+		y: number,
 	): Promise<void>;
 	fillTextToViewableElement(
 		windowId: string,
 		tabId: string,
 		x: number,
 		y: number,
-		value: string
+		value: string,
 	): Promise<void>;
 	hitEnterOnViewableElement(
 		windowId: string,
 		tabId: string,
 		x: number,
-		y: number
+		y: number,
 	): Promise<void>;
 	clickOnElement(
 		windowId: string,
 		tabId: string,
-		selector: string
+		selector: string,
 	): Promise<void>;
 	fillTextToElement(
 		windowId: string,
 		tabId: string,
 		selector: string,
-		value: string
+		value: string,
 	): Promise<void>;
 	hitEnterOnElement(
 		windowId: string,
 		tabId: string,
-		selector: string
+		selector: string,
 	): Promise<void>;
 	invokeJsFn(
 		windowId: string,
 		tabId: string,
-		fnBodyCode: string
+		fnBodyCode: string,
 	): Promise<unknown>;
 }
 
