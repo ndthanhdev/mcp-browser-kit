479f951c8d317ebcf71e92a840e7765d3bcdec34
ndthanhdev <ndthanhdev@outlook.com>
2026-06-20 20:23:55 +0000

    chore: enhance E2E workflows with Dagger setup and report merging scripts

---
+++ etc/scripts/workflows/pr-e2e-merge.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S yarn dlx tsx
+import "zx/globals";
+import path from "node:path";
+import fse from "fs-extra";
+import { workDirs } from "../utils/work-dirs";
+
+$.verbose = true;
+
+const reportDir = path.resolve(workDirs.path, "apps/ext-e2e/playwright-report");
+await fse.emptyDir(reportDir);
+
+cd(workDirs.etc.workflowRuntime.path);
+
+await $`${[
+	"dagger",
+	"call",
+	"--dir",
+	workDirs.path,
+	"build-env",
+	"with-env-variable",
+	"--name",
+	"CI",
+	"--value",
+	"true",
+	"with-moon-task",
+	"--task",
+	"scripts:playwright-install",
+	"with-moon-task",
+	"--task",
+	"ext-e2e:merge-reports",
+	"container",
+	"directory",
+	"--path=apps/ext-e2e/playwright-report",
+	"export",
+	`--path=${reportDir}`,
+]}`;
